322d01e90d0c211973099e5107a9a500663aeb63
Arina Ivanova <53275819+arinaivanova@users.noreply.github.com>
2022-01-15 18:04:30 -0500

    add network & mnist loader; update folder structure

---
+++ src/network.py
@@ -0,0 +1,13 @@
+class Network:
+
+	""" Train network using stochastic gradient descent """
+	def sgd(self)
+		pass
+	
+	"""
+	Returns number of correct output activations from network
+	
+	testing_set = (input, expected output activations)
+	"""
+	def evaluate(self, testing_set):
+		pass
